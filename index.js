ba369a55a31b70f6fb79994a6a0ed54dfaa75ae5
Jennifer Dobak <jdobak1@gmail.com>
2018-05-15 19:52:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,23 +63,24 @@
 // })
 
 
+
 function initForm() {
-  var formTemplate = document.getElementById("recipe-form-template").innerHTML
-  var template = Handlebars.compile(formTemplate)
+  let formTemplate = document.getElementById("recipe-form-template").innerHTML
+  let template = Handlebars.compile(formTemplate)
   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 }
 
 function createRecipe() {
-  var recipe = getRecipeVals()
-  var recipeTemplate = document.getElementById("recipe-template").innerHTML
-  var template = Handlebars.compile(recipeTemplate)
+  let recipe = getRecipeVals()
+  let recipeTemplate = document.getElementById("recipe-template").innerHTML
+  let template = Handlebars.compile(recipeTemplate)
   document.getElementById("main").innerHTML = template(recipe)
 }
 
 function updateRecipe() {
-  var recipe = getRecipeVals()
-  var recipeTemplate = document.getElementById("recipe-template").innerHTML
-  var template = Handlebars.compile(recipeTemplate)
+  let recipe = getRecipeVals()
+  let recipeTemplate = document.getElementById("recipe-template").innerHTML
+  let template = Handlebars.compile(recipeTemplate)
   document.getElementById("main").innerHTML = template(recipe)
 }
 //
