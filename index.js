e6aa01185eae565ab90520ec54d2dbe542e0a5ef
Jennifer Dobak <jdobak1@gmail.com>
2018-05-15 19:41:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,21 +66,21 @@ function displayEditForm() {
 //   var template = Handlebars.compile(formTemplate)
 //   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 // }
-// 
+//
 // function createRecipe() {
 //   var recipe = getRecipeVals()
 //   var recipeTemplate = document.getElementById("recipe-template").innerHTML
 //   var template = Handlebars.compile(recipeTemplate)
 //   document.getElementById("main").innerHTML = template(recipe)
 // }
-// 
+//
 // function updateRecipe() {
 //   var recipe = getRecipeVals()
 //   var recipeTemplate = document.getElementById("recipe-template").innerHTML
 //   var template = Handlebars.compile(recipeTemplate)
 //   document.getElementById("main").innerHTML = template(recipe)
 // }
-// 
+//
 // function displayEditForm() {
 //   var name = document.getElementById("nameHeader").innerText
 //   var description = document.getElementById("recipeDescription").innerText
@@ -89,14 +89,14 @@ function displayEditForm() {
 //   for(var i=0;i<ingredientsNodes.length;i++) {
 //     ingredients.push(ingredientsNodes[i].innerText)
 //   }
-// 
+//
 //   var recipe = {name, description, ingredients, submitAction: 'createRecipe()'}
-// 
+//
 //   var recipeFormTemplate = document.getElementById("recipe-form-template").innerHTML
 //   var template = Handlebars.compile(recipeFormTemplate)
 //   document.getElementById("main").innerHTML = template(recipe)
 // }
-// 
+//
 // function getRecipeVals() {
 //   var ingredientsNodes = document.getElementsByName("ingredients")
 //   var ingredients = []
@@ -110,17 +110,17 @@ function displayEditForm() {
 //   var recipe = {name, ingredients, description}
 //   return(recipe)
 // }
-// 
-// function handlebarsSetup() {
-//   //put any handlebars registrations here.
-//   Handlebars.registerHelper('displayIngredient', function(ingredient) {
-//     return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
-//   })
-//   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
-//   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
-// }
-// 
-// 
+//
+function handlebarsSetup() {
+  //put any handlebars registrations here.
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+  })
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+}
+//
+//
 // function init() {
 //   //put any page initialization/handlebars initialization here
 //   handlebarsSetup()
