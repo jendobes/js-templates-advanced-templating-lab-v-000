75897dc7b6c2b8ded0a440d351da915fbd3dfd91
Jennifer Dobak <jdobak1@gmail.com>
2018-05-15 19:15:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  compileForm()
+  renderPartials()
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
