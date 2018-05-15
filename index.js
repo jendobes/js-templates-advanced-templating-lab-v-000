5cbca05a7eeafc7cb6a94afa908d7304f52c5f11
Jennifer Dobak <jdobak1@gmail.com>
2018-05-15 19:10:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,14 @@ function getRecipeVals() {
   for(let i=0;i<singleIngredients.length;i++) {
     ingredients.push(singleIngredients[i].value)
   }
+  let name = document.getElementById("name").value
+  let description = document.getElementById("description").value
+  let recipe = {name, ingredients, description}
+  return(recipe)
 }
 
 function updateRecipe() {
-
+  let recipe = getRecipeVals()
+  let recipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML)
+  document.getElementById("main").innerHTML = recipetemplate(recipe)
 }
