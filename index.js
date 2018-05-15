e371e335569055b8401028d0aae7c35c1f638d06
Jennifer Dobak <jdobak1@gmail.com>
2018-05-15 19:50:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -97,19 +97,19 @@ function displayEditForm() {
   document.getElementById("main").innerHTML = template(recipe)
 }
 //
-// function getRecipeVals() {
-//   var ingredientsNodes = document.getElementsByName("ingredients")
-//   var ingredients = []
-//   for(var i=0;i<ingredientsNodes.length;i++) {
-//     if(ingredientsNodes[i].value !== "") {
-//       ingredients.push(ingredientsNodes[i].value)
-//     }
-//   }
-//   var name = document.getElementById("name").value
-//   var description = document.getElementById("description").value
-//   var recipe = {name, ingredients, description}
-//   return(recipe)
-// }
+function getRecipeVals() {
+  var ingredientsNodes = document.getElementsByName("ingredients")
+  var ingredients = []
+  for(var i=0;i<ingredientsNodes.length;i++) {
+    if(ingredientsNodes[i].value !== "") {
+      ingredients.push(ingredientsNodes[i].value)
+    }
+  }
+  var name = document.getElementById("name").value
+  var description = document.getElementById("description").value
+  var recipe = {name, ingredients, description}
+  return(recipe)
+}
 //
 function handlebarsSetup() {
   //put any handlebars registrations here.
