cba6c0d574db569eb81c8e131220769264bf9b32
Jennifer Dobak <jdobak1@gmail.com>
2018-05-15 19:40:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
-// function init() {
-//   //put any page initialization/handlebars initialization here
-//   compileForm()
-//   renderPartials()
-// }
-// document.addEventListener("DOMContentLoaded", function(event) {
-//   init()
-// })
-//
-//
-// function compileForm(){
-//   let formTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
-//   document.getElementsByTagName("main")[0].innerHTML = formTemplate({'submitAction': 'createRecipe()'})
-// }
-//
-// function renderPartials() {
-//   Handlebars.registerHelper('displayIngredient', function(ingredient) {
-//   return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
-// })
-//   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
-//   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
-// }
-//
-// function createRecipe() {
-//   let recipe = getRecipeVals()
-//   let recipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML)
-//   document.getElementById("main").innerHTML = recipetemplate(recipe)
-// }
-//
-// function getRecipeVals() {
-//   let singleIngredients = document.getElementsByName("ingredients")
-//   let ingredients = []
-//   for(let i=0;i<singleIngredients.length;i++) {
-//     ingredients.push(singleIngredients[i].value)
-//   }
-//   let name = document.getElementById("name").value
-//   let description = document.getElementById("description").value
-//   let recipe = {name, ingredients, description}
-//   return(recipe)
-// }
-//
-// function updateRecipe() {
-//   let recipe = getRecipeVals()
-//   let recipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML)
-//   document.getElementById("main").innerHTML = recipetemplate(recipe)
-// }
-//
-// function displayEditForm() {
-//   let name = document.getElementById("nameHeader").innerText
-//   let description = document.getElementById("recipeDescription").innerText
-//   let singleIngredients = document.getElementsByName("ingredientsList")
-//   let ingredients = []
-//   for(let i=0;i<singleIngredients.length;i++) {
-//     ingredients.push(singleIngredients[i].innerText)
-//   }
-//
-//   let recipe = {name, description, ingredients, submitAction: 'createRecipe()'}
-//
-//   let recipeFormTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
-//   document.getElementById("main").innerHTML = recipeFormTemplate(recipe)
-// }
+function init() {
+  //put any page initialization/handlebars initialization here
+  compileForm()
+  renderPartials()
+}
+document.addEventListener("DOMContentLoaded", function(event) {
+  init()
+})
+
+
+function compileForm(){
+  let formTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
+  document.getElementsByTagName("main")[0].innerHTML = formTemplate({'submitAction': 'createRecipe()'})
+}
+
+function renderPartials() {
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+  return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+})
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+}
+
+function createRecipe() {
+  let recipe = getRecipeVals()
+  let recipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML)
+  document.getElementById("main").innerHTML = recipetemplate(recipe)
+}
+
+function getRecipeVals() {
+  let singleIngredients = document.getElementsByName("ingredients")
+  let ingredients = []
+  for(let i=0;i<singleIngredients.length;i++) {
+    ingredients.push(singleIngredients[i].value)
+  }
+  let name = document.getElementById("name").value
+  let description = document.getElementById("description").value
+  let recipe = {name, ingredients, description}
+  return(recipe)
+}
+
+function updateRecipe() {
+  let recipe = getRecipeVals()
+  let recipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML)
+  document.getElementById("main").innerHTML = recipetemplate(recipe)
+}
+
+function displayEditForm() {
+  let name = document.getElementById("nameHeader").innerText
+  let description = document.getElementById("recipeDescription").innerText
+  let singleIngredients = document.getElementsByName("ingredientsList")
+  let ingredients = []
+  for(let i=0;i<singleIngredients.length;i++) {
+    ingredients.push(singleIngredients[i].innerText)
+  }
+
+  let recipe = {name, description, ingredients, submitAction: 'createRecipe()'}
+
+  let recipeFormTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML)
+  document.getElementById("main").innerHTML = recipeFormTemplate(recipe)
+}
 
 
 function initForm() {
